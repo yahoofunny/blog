c216c00cf27e1584a48cce7210e948ade5c712a0
yahoofunny <3362869734bt@gmail.com>
2026-06-14 05:52:18 +0800

    feat: 修复社交链接报错，更新专属配置，准备完美上线！

---
+++ src/content/post/firstblog.md
@@ -0,0 +1,6 @@
+---
+title: "no shame" 
+description: "fuck yiran,fuck xiaoqian, fuck yiran,fuck xiaoqian,fuck yiran" 
+publishDate: "2026-06-14"
+---
+  fuck asshole 
--- src/site.config.ts
+++ src/site.config.ts
@@ -3,18 +3,18 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
+	url: "https://blog-6rb.pages.dev/",
 	/*
 		- Used to construct the meta title property found in src/components/BaseHead.astro L:11
 		- The webmanifest name found in astro.config.ts L:42
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
+	title: "bt's Blog",
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "冰涛",
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "A blog about tech, life, and everything in my view",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
